b8c6b26cf45fa03a1c6fe51902f8786b6d42755b
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-25 13:44:36 +0200

    Started Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,13 +7,13 @@ export default function Header() {
     <div className="Header">
       <div className="row header-grid">
         <div className="col-2">
-          <img className="img-fluid" src={santa} alt="Santa Claus" />
+          <img className="img-fluid img-left" src={santa} alt="Santa Claus" />
         </div>
         <div className="col-8">
           <h1>Christmas Cracker Joke Generator</h1>
         </div>
         <div className="col-2">
-          <img className="img-fluid mr-5" src={santa} alt="Santa Claus" />
+          <img className="img-fluid img-right" src={santa} alt="Santa Claus" />
         </div>
       </div>
     </div>
